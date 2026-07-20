cc40aa8468cfbcf9d049e2c6094619eaa39b5ad2
Sadham1412 <127112415+Sadham1412@users.noreply.github.com>
2026-07-20 17:38:46 +0530

    Add files via upload

---
+++ DAY-4-TASK/find_avg.py
@@ -0,0 +1,5 @@
+maths_mark=int(input("enter you mark1"))
+science_mark=int(input("enter the mark2"))
+soical_mark=int(input("enter the mark3"))
+avg_mark=(maths_mark+science_mark+soical_mark)/3
+print("average mark=",avg_mark)
