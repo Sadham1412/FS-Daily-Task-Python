94c0b957c2159e786718b5de2480d9536a0269af
Sadham1412 <127112415+Sadham1412@users.noreply.github.com>
2026-07-23 17:37:45 +0530

    Add files via upload

---
+++ DAY-7-TASK/loops.py
@@ -0,0 +1,14 @@
+num=1
+while num<10:
+    if num%3==0:
+        print(num*10,end=" ")
+        num+=1
+    else:
+        print(num,end=" ")
+        num+=1
+# for loop
+for i in range(1,10):
+    if i%3==0:
+        print(i*10,end=" ")
+    else:
+        print(i,end=" ")
